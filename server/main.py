e947421e4d11dbccb8dbe50a7e1364ff46805a6f
Joe O'Connor <joeoc2625@gmail.com>
2020-12-26 03:42:23 +0000

    Added Google login backend

--- server/main.py
+++ server/main.py
@@ -1,8 +1,9 @@
 import logging
 import os
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, abort
 
 app = Flask(
     __name__,
@@ -14,13 +15,35 @@ app.config["DEBUG"] = os.getenv('DEBUG') == 'True'
 
 logging.basicConfig(level=logging.DEBUG if os.getenv('DEBUG') else logging.WARNING)
 
+CLIENT_ID = "389788965612-qh4j3n7fh14nfjbg7u1tmlb59mudmobj.apps.googleusercontent.com"
+
 
 @app.route('/')
 def index():
     return render_template(
         'index.html',
-        light_mode=True
+        light_mode=True,
+        user=None
     )
+
+
+@app.route('/login_google', methods=['POST'])
+def login_google():
+    json = request.get_json()
+    if 'idtoken' not in json:
+        abort(400)
+    token = json.get('idtoken')
+    idinfo = None
+    try:
+        # Specify the CLIENT_ID of the app that accesses the backend:
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+
+        print(idinfo)
+        userid = idinfo['sub']
+    except ValueError:
+        # Invalid token
+        pass
+    return idinfo
 
 
 if __name__ == "__main__":
