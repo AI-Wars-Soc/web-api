947d9f584d74c67560a8b8f1e5facfa0bf140c34
Joe O'Connor <joeoc2625@gmail.com>
2021-03-15 23:34:10 +0000

    Added error reception into webpage

--- server/static/scripts/submissions.js
+++ server/static/scripts/submissions.js
@@ -1,12 +1,27 @@
+const submission_error_box = $("#submission-error-msg");
+
 $('#submission-form').submit(function(e){
     e.preventDefault();
-    const url = $("#repo").val();
+    const repo_box = $("#repo");
+    const url = repo_box.val();
     $.post(
         {
             url: "/api/add_submission",
             data: JSON.stringify({url: url}),
             contentType: 'application/json',
             success: function(receivedData){
+                let t = receivedData._cuwais_type;
+                if (t === "submission") {
+                    submission_error_box.hide();
+                    repo_box.val("");
+                } else if (t === "error") {
+                    submission_error_box.text(receivedData.error_message);
+                    repo_box.effect("shake");
+                    submission_error_box.show();
+                } else {
+                    submission_error_box.text("Unknown return type: " + t + ". Please let Joe know");
+                    submission_error_box.show();
+                }
                 console.log(receivedData);
             }
         }
